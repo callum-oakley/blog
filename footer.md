660686c281352dcf11e334b8ca16327ad92c9d20
Callum Oakley <c.oakley108@gmail.com>
2019-06-02 01:19:42 +0100

    index

---
+++ footer.md
@@ -0,0 +1,3 @@
+Hacked together with a markdown processor and Go templates. Source [here][].
+
+[here]: https://github.com/callum-oakley/callum-oakley.github.io
--- main.go
+++ main.go
@@ -13,16 +13,41 @@ import (
 	"gopkg.in/russross/blackfriday.v2"
 )
 
-var postTemplate = template.Must(template.ParseFiles(filepath.Join("templates", "post.html")))
+var (
+	headerT = template.Must(template.ParseFiles(filepath.Join("templates", "header.html")))
+	footerT = template.Must(template.ParseFiles(filepath.Join("templates", "footer.html")))
+	postT   = template.Must(template.ParseFiles(filepath.Join("templates", "post.html")))
+	indexT  = template.Must(template.ParseFiles(filepath.Join("templates", "index.html")))
+)
+
+type index struct {
+	Title  string
+	About  string
+	Posts  []post
+	Footer string
+}
+
+func newIndex(about *bufio.Reader, footer string) (index, error) {
+	i := index{Title: "Index", Footer: footer}
+
+	b, err := ioutil.ReadAll(about)
+	if err != nil {
+		return i, err
+	}
+	i.About = string(blackfriday.Run(b))
+
+	return i, nil
+}
 
 type post struct {
 	Title   string `toml:"title"`
 	Date    string `toml:"date"`
 	Content string
+	Footer  string
 }
 
-func newPost(r *bufio.Reader) (post, error) {
-	var p post
+func newPost(r *bufio.Reader, footer string) (post, error) {
+	p := post{Footer: footer}
 	var frontMatter string
 
 	_, err := r.ReadString('\n') // discard first "+++"
@@ -75,6 +100,27 @@ func main() {
 		log.Fatal(err)
 	}
 
+	about, err := os.Open("about.md")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	f, err := os.Open("footer.md")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	b, err := ioutil.ReadAll(f)
+	if err != nil {
+		log.Fatal(err)
+	}
+	footer := string(blackfriday.Run(b))
+
+	i, err := newIndex(bufio.NewReader(about), footer)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	for _, file := range files {
 		path := filepath.Join("posts", file.Name())
 		if filepath.Ext(file.Name()) == ".md" {
@@ -83,7 +129,7 @@ func main() {
 				log.Fatal(err)
 			}
 
-			p, err := newPost(bufio.NewReader(in))
+			p, err := newPost(bufio.NewReader(in), footer)
 			if err != nil {
 				log.Fatal(err)
 			}
@@ -95,13 +141,38 @@ func main() {
 				log.Fatal(err)
 			}
 
-			if err := postTemplate.Execute(out, p); err != nil {
+			if err := headerT.Execute(out, p); err != nil {
 				log.Fatal(err)
 			}
+			if err := postT.Execute(out, p); err != nil {
+				log.Fatal(err)
+			}
+			if err := footerT.Execute(out, p); err != nil {
+				log.Fatal(err)
+			}
+
+			i.Posts = append(i.Posts, p)
 		} else {
 			if err := copyToBuild(path); err != nil {
 				log.Fatal(err)
 			}
 		}
 	}
+
+	out, err := os.Create(filepath.Join("build", "index.html"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := headerT.Execute(out, i); err != nil {
+		log.Fatal(err)
+	}
+	if err := indexT.Execute(out, i); err != nil {
+		log.Fatal(err)
+	}
+	if err := footerT.Execute(out, i); err != nil {
+		log.Fatal(err)
+	}
 }
+
+// TODO lots of repetition to factor out
